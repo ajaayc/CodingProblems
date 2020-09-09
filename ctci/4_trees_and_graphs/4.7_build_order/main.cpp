0de8954c1abbff048f86597a703714833c8b2766
Ajaay Chandrasekaran <ajaay@umich.edu>
2020-09-09 00:54:36 -0400

    4.7 buildOrder. O(V+E) time, O(V) space

--- ctci/4_trees_and_graphs/4.7_build_order/main.cpp
+++ ctci/4_trees_and_graphs/4.7_build_order/main.cpp
@@ -2,6 +2,7 @@
 #include <cassert>
 #include <vector>
 #include <unordered_map>
+#include <unordered_set>
 
 using namespace std;
 
@@ -16,16 +17,14 @@ void printVec(const std::vector<T>& vec){
 
 template <typename T>
 class Graph{
-private:
+public:
   struct Node{
     T val;
     std::vector<Node*> neighbors;
-    bool isVisited;
 
-    Node():val(T()), isVisited(false){}
-    Node(T valIn):val(valIn), isVisited(false){}
+    Node():val(T()){}
+    Node(T valIn):val(valIn){}
   };
-
 
   //Array of nodes
   std::vector<Node> nodes;
@@ -33,8 +32,7 @@ private:
 
   //Map of T to its index in the nodes array
   std::unordered_map<T,unsigned> nodeIndexMap;
-      
-public:
+
   Graph(const std::vector<T>& nodesInput, const std::vector<std::vector<T>>& edges):numNodes(nodesInput.size()){
     this->nodes.resize(nodesInput.size());
     
@@ -58,12 +56,6 @@ public:
 
   ~Graph(){}
   
-  void resetGraph(){
-    for(unsigned i = 0; i < numNodes; ++i){
-      nodes[i].isVisited = false;
-    }
-  }
-
   friend ostream& operator<<(ostream& os, const Graph<T>& g){
     for(auto n : g.nodes){
       os << n.val << ": ";
@@ -78,11 +70,86 @@ public:
   
 };
 
+/*
+O(V) space. This is a recursive algorithm, so in the worst case, the 
+dependency graph is a stick, causing the recursion to go down V levels
+O(Max(V,E)) time. This algorithm is a graph traversal algorithm. If there
+are more edges than vertices, the complexity is O(E). If there are more
+vertices than edges, the complexity is O(V).
+ */
+template <typename T>
+class Solution {
+public:
+  bool getBuildOrder(const Graph<T>& g, std::vector<T>& orderOutput){
+    std::unordered_map<T, bool> isBuilt;
+    //Populate isBuilt with all false
+    for(const typename Graph<T>::Node& currNode : g.nodes){
+      isBuilt[currNode.val] = false;
+    }
+
+    std::unordered_set<T> currPath;
+
+    bool buildError = false;
+    
+    for(unsigned i = 0; i < g.numNodes; ++i){
+      const typename Graph<T>::Node& currNode = g.nodes[i];
+      if(!isBuilt[currNode.val]){
+	  currPath.insert(currNode.val);
+	  buildNode(currNode, g, orderOutput, isBuilt, currPath, buildError);
+	  currPath.erase(currNode.val);
+
+	  if(buildError)
+	    return buildError;
+
+	  isBuilt[currNode.val] = true;
+	  orderOutput.push_back(currNode.val);
+	}
+    }
+
+    return buildError;
+  }
+private:
+  void buildNode(const typename Graph<T>::Node& currNode, const Graph<T>& g, std::vector<T>& orderOutput, std::unordered_map<T, bool>& isBuilt, std::unordered_set<T>& currPath, bool& buildError){
+
+    //Build the neighbors of N
+    for(const typename Graph<T>::Node* neighbor : currNode.neighbors){
+      if(!isBuilt[neighbor->val]){
+
+	//Check for cycle
+	if(currPath.find(neighbor->val) != currPath.end()){
+	  buildError = true;
+	  return;
+	}
+
+	currPath.insert(neighbor->val);
+	buildNode(*neighbor, g, orderOutput, isBuilt, currPath, buildError);
+	currPath.erase(neighbor->val);
+	if(buildError){
+	  return;
+	}
+
+	isBuilt[neighbor->val] = true;
+
+	orderOutput.push_back(neighbor->val);
+      }
+    }
+  }
+};
 
 template <typename T>
 void processTest(const std::vector<T>& projects, const std::vector<vector<T>> dependencies){
   Graph<T> g(projects, dependencies);
   std::cout << g << std::endl;
+  Solution<T> s;
+  std::cout << "--Build Order--" << std::endl;
+
+  std::vector<T> orderOutput;
+  bool buildErrorExists = s.getBuildOrder(g, orderOutput);
+
+  if(!buildErrorExists)
+    printVec(orderOutput);
+  else
+    std::cout << "No valid build order." << std::endl;
 }
 
 
@@ -92,7 +159,9 @@ int main(){
 
   projects = {'a','b','c','d','e','f','g'};
   dependencies = {{'d','a'},{'b','f'},{'d','b'},{'a','f'},{'c','d'}};
-
   processTest(projects, dependencies);
   
+  projects = {'a','b','c','d'};
+  dependencies = {{'a','b'},{'b','c'},{'c','d'},{'d','b'}};
+  processTest(projects, dependencies);
 }
