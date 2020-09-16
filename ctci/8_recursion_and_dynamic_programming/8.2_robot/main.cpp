1f76e657f1f03ea03f9f15cdbe28355fd696f3ee
Ajaay Chandrasekaran <ajaay@umich.edu>
2020-09-16 00:02:35 -0400

    8.2 O(r+c) memory, O(2^(r+c)) time. Can we improve the time complexity?

--- ctci/8_recursion_and_dynamic_programming/8.2_robot/main.cpp
+++ ctci/8_recursion_and_dynamic_programming/8.2_robot/main.cpp
@@ -26,17 +26,67 @@ void printVec2D(const std::vector<std::vector<T>>& vec){
 
 class Solution{
 public:
-  virtual std::vector<char> findPath(const std::vector<std::vector<bool>>& map) = 0;
+  //Returns if path exists, and if so, what the path is
+  virtual bool findPath(const std::vector<std::vector<bool>>& map, std::vector<char>& path) = 0;
 
   virtual void printMethod() = 0;
 
   virtual ~Solution(){}
 };
 
+/*
+  Let r be the number of rows in the map and c be the number of columns in the map.
+  O(r + c) memory for recursion,
+  O(2 ^ (r+c)) time to explore all possible paths from the goal node to the start node
+
+  Can we reduce the time complexity of this solution?
+ */
 class SolutionRecursive : public Solution{
 public:
-  virtual std::vector<char> findPath(const std::vector<std::vector<bool>>& map){
-    return {'r','d'};
+  virtual bool findPath(const std::vector<std::vector<bool>>& map, std::vector<char>& path){
+    int numRows = map.size(), numCols = map[0].size();
+
+    return findPathHelper(map, numRows, numCols, numRows - 1, numCols - 1, path);
+  }
+
+  //Returns if a path exists
+  virtual bool findPathHelper(const std::vector<std::vector<bool>>& map, const int numRows, const int numCols, int currRow, int currCol, std::vector<char>& path){
+    if(currRow == 0 && currCol == 0){
+      return true;
+    }
+
+    if(isValidCell(map, currRow, currCol - 1)){
+      bool leftPathFound = findPathHelper(map, numRows, numCols, currRow, currCol - 1, path);
+      if(leftPathFound){
+	path.push_back('r');
+	return true;
+      }
+    }
+
+    if(isValidCell(map, currRow - 1, currCol)){
+      bool upPathFound = findPathHelper(map, numRows, numCols, currRow - 1, currCol, path);
+      if(upPathFound){
+	path.push_back('d');
+	return true;
+      }
+    }
+
+    return false;
+  }
+
+  inline bool isValidCell(const std::vector<std::vector<bool>>& map, int r, int c){
+    return inBounds(map, r, c) && !hasObstacle(map, r, c);
+  }
+  
+  
+  inline bool inBounds(const std::vector<std::vector<bool>>& map, int r, int c){
+    int numRows = map.size(), numCols = map[0].size();
+
+    return 0 <= r && r < numRows && 0 <= c && c < numCols;
+  }
+  
+  inline bool hasObstacle(const std::vector<std::vector<bool>>& map, int r, int c){
+    return map[r][c];
   }
 
   virtual void printMethod(){
@@ -53,7 +103,12 @@ void processTest(const std::vector<std::vector<bool>>& map, std::vector<Solution
   
   for(Solution* s : methods){
     s->printMethod();
-    printVec(s->findPath(map));
+    std::vector<char> path;
+    bool foundPath = s->findPath(map, path);
+    if(foundPath)
+      printVec(path);
+    else
+      cout << "No path found!" << endl;
   }
 }
 
@@ -62,12 +117,57 @@ int main(){
   std::vector<Solution*> methods = {s1};
   
   std::vector<std::vector<bool>> map;
-  map = {{0,0,0,0,0},
-	 {0,0,0,1,0},
-	 {0,0,0,1,0},
-	 {0,0,0,1,0}};
 
-  processTest(map, methods);
+  std::vector<std::vector<std::vector<bool>>> maps;
+
+  maps.push_back({{0,0,0,0,0},
+                {0,0,0,1,0},
+                {0,0,0,1,0},
+                {0,0,0,1,0}});
+
+  maps.push_back({{0,1,0,0,0},
+                {1,0,0,1,0},
+                {0,0,0,1,0},
+                {0,0,0,1,0}});
+
+  maps.push_back({{0,1,0,1,0},
+                {1,0,0,1,0},
+                {0,0,0,1,0},
+                {0,0,0,0,0}});
+
+  maps.push_back({{0,1,0,0,0},
+                {0,1,0,1,0},
+                {0,1,0,1,0},
+                {0,0,0,0,0}});
+
+  maps.push_back({{0,1,0,0,0},
+                {0,1,0,1,0},
+                {0,1,0,1,0},
+                {0,0,0,0,0}});
+
+  maps.push_back({{0,0,1,0,0},
+                {0,1,0,1,0},
+                {0,1,0,1,0},
+                {0,0,0,0,0}});
+
+  maps.push_back({{0,0,1,1,0},
+                {1,0,0,1,1},
+                {1,1,0,0,1},
+                {0,1,1,0,0}});
+
+  maps.push_back({{0,1,1,1,0},
+                {0,0,0,0,1},
+                {1,0,1,0,1},
+		{0,0,0,0,0}});
+
+  maps.push_back({{0,1,1,1,1},
+                  {0,0,1,1,1},
+                  {1,1,0,1,1},
+		  {1,1,0,0,0}});
+
+  for(auto it = maps.begin(); it != maps.end(); ++it){
+    processTest(*it, methods);
+  }
 
   for(Solution* s : methods){
     delete s;
