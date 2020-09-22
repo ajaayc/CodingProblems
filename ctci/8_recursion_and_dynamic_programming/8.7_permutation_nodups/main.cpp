38061264762d444176d60490bdd7ec107b222f98
Ajaay Chandrasekaran <ajaay@umich.edu>
2020-09-22 00:57:54 -0400

    8.7 String Permutation. Very complex initial solution

---
+++ ctci/8_recursion_and_dynamic_programming/8.7_permutation_nodups/main.cpp
@@ -0,0 +1,105 @@
+#include <iostream>
+#include <vector>
+#include <sstream>
+
+using namespace std;
+
+template <typename T>
+void printVec(const std::vector<T>& vec){
+  for(auto it = vec.begin(); it != vec.end(); ++it){
+    std::cout << *it << " ";
+  }
+  
+  std::cout << std::endl;
+}
+
+class Solution{
+public:
+  virtual void getPermutations(const string& in, std::vector<string>& perms) = 0;
+  virtual string getMethod() = 0;
+
+  virtual ~Solution(){}
+};
+
+class SolutionDuplicateChars : public Solution{
+public:
+  virtual void getPermutations(const string& in, std::vector<string>& perms){
+    return;
+
+  }
+
+  virtual string getMethod(){
+    return "--Permutations Duplicate Chars--";
+  }
+};
+
+class SolutionNoDuplicateChars : public Solution{
+public:
+  virtual void getPermutations(const string& in, std::vector<string>& perms){
+    std::vector<string>* permptr = new vector<string>();
+    getPermutations(in, 0, permptr);
+    perms = *permptr;
+    delete permptr;
+    permptr = nullptr;
+    return;
+  }
+
+  virtual void getPermutations(const string& in, unsigned begin, std::vector<string>*& subPerms){
+    if(begin == (in.size() - 1)){
+      stringstream temp;
+      temp << in[begin];
+      subPerms->push_back(temp.str());
+      return;
+    }
+
+    getPermutations(in, begin + 1, subPerms);
+
+    //Loop through permutations, append in[currIndex] in each part
+    vector<string>* newPerms = new vector<string>();
+    for(unsigned subPermsIndex = 0; subPermsIndex < subPerms->size(); ++subPermsIndex){
+      string& currPerm = (*subPerms)[subPermsIndex];
+
+      stringstream s;
+      s << in[begin] << currPerm;
+      
+      string newPerm = s.str();
+      newPerms->push_back(newPerm);
+      
+      for(unsigned currChar = 1; currChar < newPerm.size(); ++currChar){
+	std::swap(newPerm[currChar - 1], newPerm[currChar]);
+	newPerms->push_back(newPerm);
+      }
+    }
+      
+    delete subPerms;
+    subPerms = newPerms;
+
+    return;
+  }
+
+  virtual string getMethod(){
+    return "--Permutations No Duplicate Chars--";
+  }
+};
+  
+void processTest(string in, vector<Solution*> sols){
+  for(auto it = sols.begin(); it != sols.end(); ++it){
+    std::vector<string> permutations;
+    (*it)->getPermutations(in, permutations);
+    cout << (*it)->getMethod() << endl;
+    printVec(permutations);
+  }
+}
+
+
+int main(){
+  Solution* s1 = new SolutionNoDuplicateChars();
+  //Solution* s2 = new SolutionDuplicateChars();
+  std::vector<Solution*> sols = {s1};
+
+  processTest("abcd", sols);
+
+  for(Solution* s: sols){
+    delete s;
+  }
+}
